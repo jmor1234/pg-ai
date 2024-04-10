8a6e47cf56f70e5507c5a405dc2368239d6dbad4
jmor1234 <102988961+jmor1234@users.noreply.github.com>
2024-04-10 10:14:35 -0400

    prior to trying to implement function calling

--- app/api/chat/deep-pg/route.ts
+++ app/api/chat/deep-pg/route.ts
@@ -131,7 +131,7 @@ export async function POST(req: Request) {
     console.log(`Messages: ${JSON.stringify(messages, null, 2)}`);
 
     const response = await anthropic.messages.create({
-      model: "claude-3-sonnet-20240229",
+      model: "claude-3-opus-20240229",
       stream: true,
       system: systemMessage,
       messages,
