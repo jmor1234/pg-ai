aeee5c782f0b774cc1e9862358a45b1c27b347b1
jmor1234 <102988961+jmor1234@users.noreply.github.com>
2024-04-17 10:56:54 -0400

    home page simple updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,24 +7,39 @@ const HomePage = () => {
   const { userId } = auth();
   return (
     <div className="flex flex-col h-screen items-center justify-center">
-      <div className="flex flex-col space-y-4">
+      <div className="flex flex-col space-y-2 mb-4">
         <div className="flex flex-col items-center">
           <UserButton />
         </div>
         <h1 className="text-2xl font-bold text-center">Context-PG</h1>
-        <Button className="border border-gray-300" variant="outline" asChild>
+        <Button className="border border-gray-300" variant="ghost" asChild>
           {userId ? <SignOutButton /> : <SignInButton />}
         </Button>
-        <Button className="" variant="destructive" asChild>
-          <Link href="/chats/pg-notes">Chat With Assistant</Link>
-        </Button>
-        <Button
-          className=""
-          variant="secondary"
-          asChild
-        >
-          <Link href="/notes">See Your Notes</Link>
-        </Button>
+      </div>
+      <div className="flex flex-col my-2 gap-4">
+        <div className="flex flex-col items-center justify-center my-1 gap-1">
+          <p className="max-w-prose text-center">
+            Insightful, curious and contextually relevant conversation <br></br>{" "}
+            based on Paul Graham&apos;s essays.
+          </p>
+          <Button className="max-w-fit" variant='destructive' asChild>
+            <Link href="/chats/pg-notes">Chat With Assistant</Link>
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-col my-2 gap-4">
+        <div className="flex flex-col items-center justify-center my-1 gap-1">
+          <p className="max-w-prose text-center">
+            Create notes to enhance the contextual relevance of your interaction <br></br>
+            and insights when conversing with the AI Assistant.
+          </p>
+          <Button
+            className="max-w-fit border border-primary/50"
+            asChild
+          >
+            <Link href="/notes">See Your Notes</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
